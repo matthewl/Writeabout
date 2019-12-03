9a924ad337ae569b873fdd6d4223a8c93afa9e46
Matthew Lang <matthew@matthewlang.net>
2019-12-03 21:05:00 +0000

    Add an admin API endpoint for searching for prompts

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,10 +1,18 @@
 class Prompt < ApplicationRecord
-  def self.random(the_count = 1)
-    records = offset(rand(count - the_count)).limit(the_count)
-    the_count == 1 ? records.first : records
-  end
+  RANDOM_COUNT = 1
+  LIMIT_COUNT = 10
 
   def full_prompt
     "Write about #{prompt}."
+  end
+
+  def self.random(the_count = RANDOM_COUNT)
+    records = offset(rand(count - the_count)).limit(the_count)
+    the_count == RANDOM_COUNT ? records.first : records
+  end
+
+  def self.search(query, limit_count = LIMIT_COUNT)
+    search_query = 'prompt ILIKE :query'
+    where(search_query, query: "%#{query}%").limit(limit_count).order('updated_at DESC')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   namespace :api, constraints: { format: 'json' } do
     namespace :v1 do
       resources :random_prompt, only: :index, path: 'prompts/random'
+
+      # Admin only routes
+      namespace :admin do
+        resources :prompts, only: :index
+      end
     end
   end
 end

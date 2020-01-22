0d6ad238d497486bf54835877070bed846b6d9d5
Matthew Lang <matthew@matthewlang.net>
2020-01-22 07:07:49 +0000

    🌟 Progressive enhance the copy to clipboard feature (#16)
    * Add selectors to enhance copy button
    * Ensure the button displays when JS is ready

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,6 +3,12 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "source" ]
 
+  connect() {
+    if (document.queryCommandSupported("copy")) {
+      this.element.classList.add("clipboard--supported")
+    }
+  }
+
   copy() {
     var prompt = document.getElementById('prompt').getAttribute('data-text')
     const el = document.createElement('textarea')

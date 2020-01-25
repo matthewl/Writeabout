ab9882e1cf5e94c26eba7f53e578a57e9ec12b7c
Matthew Lang <matthew@matthewlang.net>
2020-01-25 11:22:57 +0000

    Persist the theme through a cookie

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -6,20 +6,65 @@ export default class extends Controller {
   connect() {
     if (document.queryCommandSupported("copy")) {
       this.buttonTarget.classList.add("theme--supported")
+      this.readCookieTheme()
     }
   }
 
   switch() {
-    const body = document.body
+    let body = document.body
 
     if (body.classList.contains('light')) {
-      body.classList.remove('light')
-      body.classList.add('dark')
+      this.switchToDark()
+      this.writeCookieTheme('dark')
     }
     else
     {
-      body.classList.remove('dark')
-      body.classList.add('light')
+      this.switchToLight()
+      this.writeCookieTheme('light')
+    }
+  }
+
+  switchTheme(theme) {
+    if (theme == 'dark') {
+      this.switchToDark()
+      this.writeCookieTheme('dark')
+    }
+    else
+    {
+      this.switchToLight()
+      this.writeCookieTheme('light')
+    }
+  }
+
+  switchToLight() {
+    let body = document.body
+    body.classList.remove('dark')
+    body.classList.add('light')
+  }
+
+  switchToDark() {
+    let body = document.body
+    body.classList.remove('light')
+    body.classList.add('dark')
+  }
+
+  writeCookieTheme(theme) {
+    let now = new Date()
+    now.setMonth( now.getYear() + 10 )
+    document.cookie = "theme=" + theme
+    document.cookie = "expires=" + now.toUTCString() + ";"
+  }
+
+  readCookieTheme() {
+    const allcookies = document.cookie
+    const cookiearray = allcookies.split(';')
+    
+    for(var i=0; i<cookiearray.length; i++) {
+      const name = cookiearray[i].split('=')[0].trim()
+      const value = cookiearray[i].split('=')[1].trim()
+      if (name == 'theme') {
+        this.switchTheme(value)
+      }
     }
   }
 }

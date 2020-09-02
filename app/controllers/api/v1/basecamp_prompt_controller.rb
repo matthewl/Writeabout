30f2d786b288a0af4f82fbc1448741d16ec03fd5
Matthew Lang <matthew@matthewlang.net>
2020-09-02 11:32:05 +0100

    Disable authenticity check for Basecamp integration

--- app/controllers/api/v1/basecamp_prompt_controller.rb
+++ app/controllers/api/v1/basecamp_prompt_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class BasecampPromptController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
       def create
         @prompt = Prompt.random
         render :prompt, layout: false

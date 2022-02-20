cf03f81e52787c7d4ae3f93623acc7fbe9b56781
Matthew Lang <matthewlang@hey.com>
2022-02-20 16:54:14 +0000

    Add prompts controller and view

---
+++ test/controllers/prompts_controller_test.rb
@@ -0,0 +1,6 @@
+class PromptsControllerTest < ActionDispatch::IntegrationTest
+  test 'show should respond successfully' do
+    get prompt_url(prompts(:fonts).public_id)
+    assert_response :success
+  end
+end

58543bb17bb73d9d3bd55644c9718d77eebf79a5
Matthew Lang <matthewlang@hey.com>
2022-02-27 21:24:58 +0000

    Skip authentication token check for API

--- app/controllers/api/v1/random_prompt_controller.rb
+++ app/controllers/api/v1/random_prompt_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class RandomPromptController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
       def index
         render json: Prompt.random.slice(:prompt, :full_prompt)
       end

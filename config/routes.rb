ff5b92d0a31b9eb5f642bd1451023f2bda231835
Matthew Lang <matthewlang@hey.com>
2022-06-10 07:52:23 +0100

    Add the single admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
-  match '/404', to: 'errors#not_found', via: :all, as: 'not_found'
-  match '/500', to: 'errors#internal_server_error', via: :all, as: 'internal_server_error'
+  match "/404", to: "errors#not_found", via: :all, as: "not_found"
+  match "/500", to: "errors#internal_server_error", via: :all, as: "internal_server_error"
 
-  root 'home#show'
+  root "home#show"
   resources :prompts, only: :show
-  get '/feed', to: 'home#show', defaults: { format: 'rss' }
+  get "/feed", to: "home#show", defaults: {format: "rss"}
 
-  namespace :admin
-    resource :home, only: :index
-  end
+  resources :admin, only: :index
 
   # API routes
-  namespace :api, constraints: { format: 'json' } do
+  namespace :api, constraints: {format: "json"} do
     namespace :v1 do
-      resources :random_prompt, only: :index, path: 'prompts/random'
-      resources :basecamp_prompt, only: :create, path: 'prompts/basecamp'
+      resources :random_prompt, only: :index, path: "prompts/random"
+      resources :basecamp_prompt, only: :create, path: "prompts/basecamp"
 
       # Admin only routes
       namespace :admin do

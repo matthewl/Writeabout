bda96b029ea2062aa66e35cf435dc444b9065e61
Matthew Lang <matthewlang@hey.com>
2024-08-15 17:20:56 +0100

    Remove Trestle behaviour

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,7 +1,4 @@
 class Administrator < ApplicationRecord
-  include Trestle::Auth::ModelMethods
-  include Trestle::Auth::ModelMethods::Rememberable
-
   has_secure_password
   validates :email, presence: true, uniqueness: true
 end
